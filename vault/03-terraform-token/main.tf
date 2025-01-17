122dfe22b7ccd6f063593b91b5c5ec9e68fec29b
Julien WITTOUCK <julien.wittouck@gmail.com>
2025-01-17 17:19:34 +0100

    🎨 : format code

--- vault/03-terraform-token/main.tf
+++ vault/03-terraform-token/main.tf
@@ -1,11 +1,11 @@
 resource "vault_policy" "terraform" {
-  name = "terraform-policy"
+  name   = "terraform-policy"
   policy = file("terraform-policy.hcl")
 }
 
 resource "vault_token" "terraform_token" {
   display_name = "terraform-token"
-  policies = ["terraform-policy"]
+  policies     = ["terraform-policy"]
 
   ttl       = "24h"
   renewable = true
